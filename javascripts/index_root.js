8d694fb45756f6ae1480486c72ecbca53a9c1297
yjein <yjein4220@raonfreinds.com>
2021-03-26 10:31:39 +0900

    [수정] index.html logo-wrapper 컴포먼트화

--- javascripts/index_root.js
+++ javascripts/index_root.js
@@ -2,5 +2,8 @@ const Root = () => {
   const root = document.querySelector(".root")
   const header = document.createElement("header")
   root.appendChild(header)
+  const logo_wrap = document.createElement("div")
+  logo_wrap.classList.add("logo-wrapper")
+  header.appendChild(logo_wrap)
 }
 Root()
