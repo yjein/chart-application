154be1ee2f8b87e5591d8eac33dfbaed2098e002
yjein <yjein4220@raonfreinds.com>
2021-03-26 18:16:08 +0900

    [추가] index.html dashboard for문 추가

--- javascripts/index/main/index_dashboard.js
+++ javascripts/index/main/index_dashboard.js
@@ -1,8 +1,13 @@
+const tempData = []
+for (let i = 0; i < 15; i++) {
+  tempData.push(i + 1)
+}
+
 const dashboard = () => {
   const dashboard = document.createElement("div")
   dashboard.classList.add("dashboard")
 
-  dashboard.appendChild(img())
+  tempData.forEach((item) => dashboard.appendChild(img()))
 
   return dashboard
 }
