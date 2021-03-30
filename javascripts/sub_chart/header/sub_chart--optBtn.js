a6cb4c50620e4eb742153e45144095b03017ecf4
임 한결 <hanbig@raonfriends.com>
2021-03-30 23:46:49 +0000

    [추가] TODO 추가

--- javascripts/sub_chart/header/sub_chart--optBtn.js
+++ javascripts/sub_chart/header/sub_chart--optBtn.js
@@ -4,7 +4,7 @@ const optBtn = () => {
   const opt_button = document.createElement("button")
   opt_button.classList.add("optBtn")
   opt_button.textContent = "="
-
+	//TODO: 의존성 수정 필요
   const toggleTheBar = () => {
     document.querySelector(".chartOption").classList.toggle("--close")
   }
