363299333b628d4b7918a30bc5daa2f1a3f70778
Saechan <saechan@raonfriends.com>
2021-04-02 16:45:49 +0900

    [수정] : 옵션바 이벤트처리 수정
    -옵션바 클릭 이벤트 처리시 옵션바가 화면 밖으로 벗어나는 오류 해결

--- javascripts/sub_chart/sub_chart_CreateChart.js
+++ javascripts/sub_chart/sub_chart_CreateChart.js
@@ -5,8 +5,23 @@ const CreateChart = (option) => {
   create_chart.classList.add("createChart")
 
   setTimeout(() => {
-    const myChart = echarts.init(create_chart)
+    const myChart = echarts.init(
+      create_chart,
+      {},
+      {
+        renderer: "svg",
+      }
+    )
+
     option && myChart.setOption(option)
+
+    const ro = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        myChart.resize()
+        if (entry.target.handleResize) entry.target.handleResize(entry)
+      }
+    })
+    ro.observe(create_chart)
   }, 0)
 
   return create_chart
