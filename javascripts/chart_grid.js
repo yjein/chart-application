addf3f368e0627fa2a84dd442861ed3ef24ca82f
Saechan <saechan@raonfriends.com>
2021-04-06 17:59:25 +0900

    [추가] : 그리드 테마적용

--- javascripts/chart_grid.js
+++ javascripts/chart_grid.js
@@ -4,6 +4,7 @@ const chart_grid = (gridOption) => {
   const chart_grid = document.createElement("div")
 
   chart_grid.classList.add("ag_grid")
+  chart_grid.classList.add("ag-theme-alpine")
   new agGrid.Grid(chart_grid, gridOption)
 
   return chart_grid
