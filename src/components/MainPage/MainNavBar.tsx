8fd5dd9bbf377b744439ba29e1b2f92dc31ddc36
Saechan <saechan@raonfriends.com>
2021-04-15 11:39:41 +0900

    [추가] : 메인페이지 nav바 컴포넌트화

---
+++ src/components/MainPage/MainNavBar.tsx
@@ -0,0 +1,12 @@
+import { Nav } from "../../style/main"
+import MainNavList from "./MainNavList"
+
+const MainNavBar = () => {
+  return (
+    <Nav>
+      <MainNavList />
+    </Nav>
+  )
+}
+
+export default MainNavBar
