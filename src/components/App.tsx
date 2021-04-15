0411616bb92715babfaa27924307aaf5a865c0d3
yjein <yjein4220@raonfreinds.com>
2021-04-15 18:06:33 +0900

    [수정] 아토믹 디자인으로 재설계 5단계 templates 컴포넌트화
    - App.tsx 파일 이동 후 import 경로 수정

--- src/components/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import MainPage from "./MainPage"
-
-const App = () => {
-  return <MainPage />
-}
-
-export default App
---
+++ src/components/ChartPage/Pages/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import MainPage from "../../MainPage"
+import ChartPage from "../Templates/chartPage"
+
+const App = () => {
+  return (
+    <>
+      {/* <MainPage /> */}
+      <ChartPage />
+    </>
+  )
+}
+
+export default App
