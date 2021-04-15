f95683ef8e8423cd1e477808fea3d368b881aac7
Saechan <saechan@raonfriends.com>
2021-04-15 19:06:55 +0900

    [수정] : App파일 MainPage폴더로 이동

--- src/components/ChartPage/Pages/App.tsx
+++ src/components/ChartPage/Pages/App.tsx
@@ -1,14 +1,9 @@
 import React from "react"
-import MainPage from "../../MainPage"
+
 import ChartPage from "../Templates/chartPage"
 
 const App = () => {
-  return (
-    <>
-      {/* <MainPage /> */}
-      <ChartPage />
-    </>
-  )
+  return <ChartPage />
 }
 
 export default App
--- src/components/MainPage/App.tsx
+++ src/components/MainPage/App.tsx
@@ -1,4 +1,4 @@
-import MainPage from "./MainPage/Page/MainPage"
+import MainPage from "./Page/MainPage"
 
 const App = () => {
   return <MainPage />
