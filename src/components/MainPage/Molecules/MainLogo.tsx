542c53c44e171b4b5c97779bbdf85f0d75e0f4ae
Saechan <saechan@raonfriends.com>
2021-04-15 16:10:42 +0900

    [추가] : Molecule컴포넌트화(메인로고)

---
+++ src/components/MainPage/Molecules/MainLogo.tsx
@@ -0,0 +1,12 @@
+import LogoBg from "../Atoms/LogoBg"
+import Logo from "../Atoms/Logo"
+
+const MainLogo = () => {
+  return (
+    <LogoBg>
+      <Logo>RaonChart</Logo>
+    </LogoBg>
+  )
+}
+
+export default MainLogo
