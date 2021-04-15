9e641eb52a7703db7b5ac6b14d0823b8f9785d75
Saechan <saechan@raonfriends.com>
2021-04-15 11:24:30 +0900

    [추가] : 메인페이지 Logo 컴포넌트화

---
+++ src/components/MainPage/MainLogo.tsx
@@ -0,0 +1,11 @@
+import { LogoBg, Logo } from "../../style/main"
+
+const MainLogo = () => {
+  return (
+    <LogoBg>
+      <Logo>RaonChart</Logo>
+    </LogoBg>
+  )
+}
+
+export default MainLogo
