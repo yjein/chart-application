5233446106f37be6efd4969a1a72ee01909744cf
Saechan <saechan@raonfriends.com>
2021-04-22 18:16:36 +0900

    [추가] : 차트이미지 컴포넌트화

---
+++ src/assets/ChartData.tsx
@@ -0,0 +1,29 @@
+import barUrl from "./charts/bar.png"
+import lineUrl from "./charts/line.png"
+import candlestickUrl from "./charts/candlestick.png"
+import gaugeUrl from "./charts/gauge.png"
+import funnelUrl from "./charts/funnel.png"
+import parallelUrl from "./charts/parallel.png"
+import pictorialbarUrl from "./charts/pictorialbar.png"
+import pieUrl from "./charts/pie.png"
+import radarUrl from "./charts/radar.png"
+import sankeyUrl from "./charts/sankey.png"
+import scatterUrl from "./charts/scatter.png"
+import sunburstUrl from "./charts/sunburst.png"
+
+const Urls = [
+  barUrl,
+  lineUrl,
+  candlestickUrl,
+  gaugeUrl,
+  funnelUrl,
+  parallelUrl,
+  pictorialbarUrl,
+  pieUrl,
+  radarUrl,
+  sankeyUrl,
+  scatterUrl,
+  sunburstUrl,
+]
+
+export default Urls
--- src/components/Organisms/dashBoard.tsx
+++ src/components/Organisms/dashBoard.tsx
@@ -1,33 +1,6 @@
 import styled from "styled-components"
 import Image from "../Atoms/img"
-
-import barUrl from "../../assets/charts/bar.png"
-import lineUrl from "../../assets/charts/line.png"
-import candlestickUrl from "../../assets/charts/candlestick.png"
-import gaugeUrl from "../../assets/charts/gauge.png"
-import funnelUrl from "../../assets/charts/funnel.png"
-import parallelUrl from "../../assets/charts/parallel.png"
-import pictorialbarUrl from "../../assets/charts/pictorialbar.png"
-import pieUrl from "../../assets/charts/pie.png"
-import radarUrl from "../../assets/charts/radar.png"
-import sankeyUrl from "../../assets/charts/sankey.png"
-import scatterUrl from "../../assets/charts/scatter.png"
-import sunburstUrl from "../../assets/charts/sunburst.png"
-
-const Urls = [
-  barUrl,
-  lineUrl,
-  candlestickUrl,
-  gaugeUrl,
-  funnelUrl,
-  parallelUrl,
-  pictorialbarUrl,
-  pieUrl,
-  radarUrl,
-  sankeyUrl,
-  scatterUrl,
-  sunburstUrl,
-]
+import ChartData from "../../assets/ChartData"
 
 const DashBord = styled.div`
   display: grid;
@@ -49,7 +22,7 @@ const Div = styled.div`
 const MainDashBord = () => {
   return (
     <DashBord>
-      {Urls.map((item) => (
+      {ChartData.map((item) => (
         <Div>
           <Image url={item} />
         </Div>
