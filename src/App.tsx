58db82f425b9138fe41281e6451e78053b035e96
yjein <yjein4220@raonfreinds.com>
2021-04-22 11:19:32 +0900

    [추가] App.tsx 추가

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import MainPage from "./components/Pages/mainPage"
+import ChartPage from "./components/Pages/chartPage"
+
+const App = () => {
+    return (
+        <>
+            <MainPage/>
+            <ChartPage/>
+        </>
+    )
+}
+
+export default App
