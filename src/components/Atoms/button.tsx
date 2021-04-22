a74170cfd0ab287343bab03216e739d10a70b495
yjein <yjein4220@raonfreinds.com>
2021-04-22 12:23:08 +0900

    [수정] 버튼 2차수정
    -오류나는 부분 삭제

--- src/components/Atoms/button.tsx
+++ src/components/Atoms/button.tsx
@@ -2,21 +2,19 @@ import React from "react"
 import styled from "styled-components"
 
 interface Props {
-  border?: string
-  onClick?: React.MouseEventHandler<HTMLDivElement>
+  // border?: string
+  // onClick?: React.MouseEventHandler<HTMLDivElement>
 }
 
-const Button: React.FC<{}> = (props, { border, onClick }) => {
+const Button: React.FC<{}> = (props) => {
   const { children } = props
-  return <StyleButton border={border} onClick={onClick}>{children}</StyleButton>
+  return <StyleButton></StyleButton>
 }
 
 const StyleButton = styled.div`
   margin: 0.5rem;
   width: 1.5rem;
   height: 1.5rem;
-  border: ${(props: Props) =>
-    props.border === "none" ? "none" : `0.25rem solid #333333`};
   border-radius: 0.5rem;
   cursor: pointer;
 `
