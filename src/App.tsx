a95223579061a4f47f8b58ca6f87caacc53513e5
Saechan <saechan@raonfriends.com>
2021-04-23 11:06:17 +0900

    [추가] : useState를 이용한 페이지 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from "react"
-import MainPage from "./components/Pages/mainPage"
+import React, { useState } from "react"
+import MainPage from "./components/Pages/MainPage"
 import ChartPage from "./components/Pages/ChartPage"
 
+type Pages = "MainPage" | "ChartPage"
+
 const App = () => {
+  const [PageState, SetPageState] = useState<Pages>("MainPage")
+
   return (
     <>
-      <MainPage />
-      <ChartPage />
+      {PageState === "MainPage" && <MainPage />}
+      {PageState === "ChartPage" && <ChartPage />}
     </>
   )
 }
