2d29b99205afd93002fafd7bfa3c29cd66b47533
Saechan <saechan@raonfriends.com>
2021-04-23 10:27:13 +0900

    [추가] : DashBord이름변경을 위한 추가

---
+++ src/components/Organisms/DashBord.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+import Image from "../Atoms/Img"
+import ChartData from "../../assets/ChartData"
+
+const DashBord = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 0.5rem;
+`
+
+const Div = styled.div`
+  &:nth-child(1) {
+    grid-column: 1 / span 2;
+    grid-row: 1 / span 2;
+  }
+  &:nth-child(2) {
+    grid-column: 3 / span 2;
+    grid-row: 2 / span 2;
+  }
+`
+
+const MainDashBord = () => {
+  return (
+    <DashBord>
+      {ChartData.map((item) => (
+        <Div>
+          <Image url={item} />
+        </Div>
+      ))}
+    </DashBord>
+  )
+}
+
+export default MainDashBord
