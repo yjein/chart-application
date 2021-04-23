6b4c4c0fa9040691f08f2ce99dfddba13627a092
yjein <yjein4220@raonfreinds.com>
2021-04-23 17:44:05 +0900

    [수정] 네비게이션바 크기에 맞춰 사이드바 길이 조정

--- src/components/Templates/ChartAside.tsx
+++ src/components/Templates/ChartAside.tsx
@@ -4,8 +4,7 @@ import styled from "styled-components"
 const StyleChartAside = styled.div`
   position: sticky;
   top: 0;
-  width: 200px;
-  height: calc(100vh - 40px);
+  height: calc(100vh - 2.75rem);
   overflow-x: auto;
   background-color: #c9c9c9;
 
