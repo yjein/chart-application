e44248d6548b33f5bf385e5b722f4a4a28bba798
yjein <yjein4220@raonfreinds.com>
2021-04-23 19:14:11 +0900

    [추가] Button 파일 추가

---
+++ src/components/Atoms/Button.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import styled from "styled-components"
+
+// interface Props {
+// border?: string
+// onClick?: React.MouseEventHandler<HTMLDivElement>
+// }
+
+const StyleButton = styled.div`
+  padding: 0.5rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+`
+
+const Button: React.FC<{}> = (props) => {
+  return <StyleButton>{props.children}</StyleButton>
+}
+
+export default Button
