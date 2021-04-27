4791996e0dcc95e1567005149dff609624ed6eb8
Saechan <saechan@raonfriends.com>
2021-04-27 12:18:44 +0900

    [수정] : line차트 데이터수정

--- src/assets/Options/lineOption.ts
+++ src/assets/Options/lineOption.ts
@@ -1,16 +1,40 @@
 const LineOption = {
+  title: {
+    left: "center",
+    text: "주간 신규 코로나 바이러스 확진자수",
+  },
+  tooltip: {},
+  toolbox: {
+    show: true,
+    feature: {
+      dataView: { show: true, readOnly: false },
+      restore: { show: true },
+      saveAsImage: { show: true },
+    },
+  },
   xAxis: {
     type: "category",
-    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    data: [
+      "월요일",
+      "화요일",
+      "수요일",
+      "목요일",
+      "금요일",
+      "토요일",
+      "일요일",
+    ],
   },
   yAxis: {
     type: "value",
   },
   series: [
     {
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      data: [282, 347, 406, 451, 557, 543, 443],
       type: "line",
       smooth: true,
+      lineStyle: {
+        width: 5,
+      },
     },
   ],
 }
