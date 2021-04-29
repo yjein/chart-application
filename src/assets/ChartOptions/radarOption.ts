b885b6c78b9e1d5de08ad455ead4588b7d89b339
Saechan <saechan@raonfriends.com>
2021-04-29 11:51:25 +0900

    [수정] : Radar 차트 옵션 수정

--- src/assets/ChartOptions/radarOption.ts
+++ src/assets/ChartOptions/radarOption.ts
@@ -1,7 +1,4 @@
 const Radar = {
-  title: {
-    text: "基础雷达图",
-  },
   tooltip: {},
   toolbox: {
     show: true,
@@ -13,7 +10,7 @@ const Radar = {
     },
   },
   legend: {
-    data: ["预算分配（Allocated Budget）", "实际开销（Actual Spending）"],
+    data: ["예산할당", "실제예산"],
   },
   radar: {
     // shape: 'circle',
@@ -26,12 +23,12 @@ const Radar = {
       },
     },
     indicator: [
-      { name: "销售（sales）", max: 6500 },
-      { name: "管理（Administration）", max: 16000 },
-      { name: "信息技术（Information Techology）", max: 30000 },
-      { name: "客服（Customer Support）", max: 38000 },
-      { name: "研发（Development）", max: 52000 },
-      { name: "市场（Marketing）", max: 25000 },
+      { name: "매상", max: 6500 },
+      { name: "관리", max: 16000 },
+      { name: "정보기술", max: 30000 },
+      { name: "고객 서비스", max: 38000 },
+      { name: "개발", max: 52000 },
+      { name: "마케팅", max: 25000 },
     ],
   },
   series: [
@@ -42,11 +39,11 @@ const Radar = {
       data: [
         {
           value: [4300, 10000, 28000, 35000, 50000, 19000],
-          name: "预算分配（Allocated Budget）",
+          name: "예산할당",
         },
         {
           value: [5000, 14000, 28000, 31000, 42000, 21000],
-          name: "实际开销（Actual Spending）",
+          name: "실제예산",
         },
       ],
     },
