b62dd5db02a73f1677402a6b56bf572013ccc051
Saechan <saechan@raonfriends.com>
2021-04-29 11:15:45 +0900

    [수정] : Funnel차트 그리드 옵션 수정

--- src/assets/GridOptions/funnelGrid.ts
+++ src/assets/GridOptions/funnelGrid.ts
@@ -1,12 +1,25 @@
 export const funnelGrid = {
   columnDefs: [
-    { headerName: "Make", field: "make" },
-    { headerName: "Model", field: "model" },
-    { headerName: "Price", field: "price" },
+    { headerName: "인식", field: "인식" },
+    { headerName: "관심", field: "관심" },
+    { headerName: "고려", field: "고려" },
+    { headerName: "의지", field: "의지" },
+    { headerName: "구매", field: "구매" },
   ],
   rowData: [
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 },
+    {
+      인식: "100% - 예상",
+      관심: "80% - 예상",
+      고려: "60% - 예상",
+      의지: "40% - 예상",
+      구매: "20% - 예상",
+    },
+    {
+      인식: "80% - 실제",
+      관심: "50% - 실제",
+      고려: "30% - 실제",
+      의지: "10% - 실제",
+      구매: "5% - 실제",
+    },
   ],
 }
