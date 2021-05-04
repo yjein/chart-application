7ef2e6782f8c4b4e605ab7f6f4e9e4ef99a94371
yjein <yjein4220@raonfreinds.com>
2021-05-04 14:00:12 +0900

    [수정] lineData 그리드값 수정

--- src/assets/ChartDatas/lineData.ts
+++ src/assets/ChartDatas/lineData.ts
@@ -1,12 +1,14 @@
 export const lineData = {
   columnDefs: [
-    { headerName: "Make", field: "make" },
-    { headerName: "Model", field: "model" },
-    { headerName: "Price", field: "price" },
+    { headerName: "월요일", field: "mon" },
+    { headerName: "화요일", field: "tue" },
+    { headerName: "수요일", field: "wed" },
+    { headerName: "목요일", field: "thu" },
+    { headerName: "금요일", field: "fri" },
+    { headerName: "토요일", field: "sat" },
+    { headerName: "일요일", field: "sun" },
   ],
   rowData: [
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 },
+    { mon: 282, tue: 347, wed: 406, thu: 451, fri: 557, sat: 543, sun: 443},
   ],
 }
